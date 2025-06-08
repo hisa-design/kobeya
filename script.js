602541db6338e3fd6125239a39064544f510ae53
hisa-design <yona407.design@gmail.com>
2025-06-09 07:27:56 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    document.querySelector('.door').classList.add('open');
+  }, 500);
+});
+
+console.log("ようこそ！ひみつのこべやへ✨");
+
+// データ取得と表示
+window.addEventListener('DOMContentLoaded', function() {
+  Tabletop.init({
+    key: '1VvJ1ySSarudBRJjvsROCY2TelcoTDZ89VO02ZUC4Nqk',
+    callback: function(data, tabletop) {
+      console.log("取得したデータ：", data);
+      showLatestData(data);
+    },
+    simpleSheet: true
+  });
+});
+
+function showLatestData(data) {
+  console.log("データを表示するよ！", data);
+
+  // 最新データを1件取得
+  const latestEntry = data[data.length - 1];
+
+  // 表示する要素を作る
+  const diarySection = document.getElementById("diary-section");
+
+  const card = document.createElement("div");
+  card.className = "diary-card";
+
+  card.innerHTML = `
+    <div class="diary-date">${latestEntry.date}</div>
+    <img src="img/${latestEntry.photo}" class="diary-photo" alt="">
+    <div class="diary-memo">${latestEntry.memo}</div>
+  `;
+
+  diarySection.appendChild(card);
+}
